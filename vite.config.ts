8059dcd2cde975da217d489bf83bb7635cc9b79a
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-07-22 14:56:59 -0300

    fix: serverless function

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
-
-  return {
-    plugins: [react()],
-    server: isDevelopment ? {
-      proxy: {
-        '/api': {
-          target: 'https://geocoding.geo.census.gov',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://geocoding.geo.census.gov',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
-    } : {}
-  };
-});
+    }
+  }
+})
